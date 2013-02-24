2126b81c216a27bde15cfd89dffc5168c27d62bc
Artem-PC <kiler4747@gmail.com>
2013-02-24 22:00:57 +0300

    Bubble sort

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
 
 namespace BubbleSort
 {
@@ -9,7 +7,31 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
-            
+            int n = 100;
+            Random rnd = new Random();
+            int[] mass = new int[n];
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next();
+            }
+
+            bool isSwap;
+            do
+            {
+                isSwap = false;
+                for (int i = 0; i < mass.Length - 1; i++)
+                    if (mass[i] > mass[i + 1])
+                    {
+                        var swap = mass[i];
+                        mass[i] = mass[i + 1];
+                        mass[i + 1] = swap;
+                        isSwap = true;
+                    }
+            } while (isSwap);
+
+            foreach (var element in mass)
+                Console.WriteLine(element);
         }
     }
 }
