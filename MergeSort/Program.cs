f2da7c5f23ef28c9dcda53998695ca370f7026f2
Artem-PC <kiler4747@gmail.com>
2013-03-05 22:33:44 +0300

    complete

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -37,29 +37,41 @@ namespace MergeSort
 
 		static private void Merge(int[] mass, int start, int middle, int end)
 		{
+			if (mass == null) 
+				throw new ArgumentNullException("mass");
 			int leftCount = middle - start + 1;
 			int rightCount = end - middle;
 			int[] left = new int[leftCount];
 			int[] right = new int[rightCount];
 			int i = 0, j = 0;
 
+
 			for (i = 0; i < left.Length; i++)
 				left[i] = mass[i + start];
 			for (j = 0; j < right.Length; j++)
 				right[j] = mass[middle + j+1];
-			//left[leftCount] = left.Max() + 1;
-			//right[rightCount] = right.Max() + 1;
 			
 			i = j = 0;
 			int k;
-			for (k = start; k < end && i != left.Length && j != right.Length; k++)
+			for (k = start; i!= left.Length || j!= right.Length; k++)
 			{
 				//if (i != left.Length & j != right.Length)
 				//{
-					if (left[i] <= right[j])
+				if ((i == left.Length) && (j < right.Length))
+					mass[k] = right[j++];
+				else 
+				{
+					if ((j == right.Length) && (i < left.Length))
 						mass[k] = left[i++];
 					else
-						mass[k] = right[j++];
+					{
+						if (left[i] <= right[j])
+							mass[k] = left[i++];
+						else
+							mass[k] = right[j++];
+					}
+				}
+
 				//}
 				//else
 				//{
@@ -71,16 +83,15 @@ namespace MergeSort
 				//    }
 				//}
 			}
-			k--;
-			if (i == left.Length)
-				for (; k < end; k++)
-					mass[k] = right[j++];
-			else
-			{
-				for (; k < end; k++)
-					mass[k] = left[i++];
-			}
+			//k--;
+			//if (i == left.Length)
+			//    for (; k < end; k++)
+			//        mass[k] = right[j++];
+			//else
+			//{
+			//    for (; k < end; k++)
+			//        mass[k] = left[i++];
+			//}
 		}
-
 	}
 }
