b1dfe2fa86a20fe27741efa32b222ab9b6353c1f
Artem-PC <kiler4747@gmail.com>
2013-03-05 23:55:27 +0300

    Singleton

---
+++ Singl/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singl
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Singleton x1 = Singleton.Instance;
+			Singleton x2 = Singleton.Instance;
+			if (x1 == x2)
+				Console.WriteLine("Объекты равны");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Singl/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singl
+{
+	class Singleton
+	{
+		static readonly Singleton instance = new Singleton();
+
+		Singleton()
+		{
+		}
+
+		static Singleton()
+		{
+			
+		}
+
+		public static Singleton Instance
+		{
+			get { return instance; }
+		}
+	}
+}
