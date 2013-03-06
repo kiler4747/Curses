c2ae51c5ab85a2a1645ac02d7fa2020e62fe87be
Artem-PC <kiler4747@gmail.com>
2013-03-06 12:34:24 +0300

    add constructors

--- HugeInt/UHugeInt.cs
+++ HugeInt/UHugeInt.cs
@@ -7,5 +7,28 @@ namespace HugeInt
 {
 	class UHugeInt
 	{
+		private byte[] digits;
+
+		public UHugeInt()
+			: this(0)
+		{
+		}
+
+		public UHugeInt(int x)
+		{
+			string number = x.ToString();
+			int countDigits = x.ToString().Length;
+			digits = new byte[countDigits];
+			for (int i = 0; i < number.Length; i++)
+			{
+				digits[i] = byte.Parse((number[i]).ToString());
+			}
+		}
+
+		public UHugeInt(string str)
+			: this(int.Parse(str))
+		{
+			
+		}
 	}
 }
