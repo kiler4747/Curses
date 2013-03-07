facd5c6fd40c8eb2795b460988132408ac073c13
Artem-PC <kiler4747@gmail.com>
2013-03-07 22:30:05 +0300

    IComparable :
    	CompareTo

---
+++ HugeInt/HugeInt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HugeInt
+{
+	class HugeInt : UHugeInt, IComparable
+	{
+		public int CompareTo(object obj)
+		{
+			HugeInt right = (HugeInt) obj;
+			if (right == null)
+				return 0;
+			if (this < right)
+				return -1;
+			if (this > right)
+				return 1;
+			return 0;
+		}
+	}
+}
