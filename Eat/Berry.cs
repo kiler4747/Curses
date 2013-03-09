8ae373b76e4e5ef8d0a89a01d68e489fffe42876
Artem-PC <kiler4747@gmail.com>
2013-03-09 19:54:12 +0300

    Berry

---
+++ Eat/Berry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eat
+{
+	class Berry : IEat
+	{
+		public enum Berries
+		{
+			Cowberry,
+			Blueberries,
+			Cherry
+		}
+
+		private Berries berry;
+
+		public Berry(Berries berry)
+		{
+			this.berry = berry;
+		}
+
+		public bool Eat()
+		{
+			if (berry == Berries.Cherry)
+				return true;
+			return false;
+		}
+	}
+}
