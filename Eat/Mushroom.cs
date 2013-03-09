f1b70482b9da1c70496d4282b5b8cb8de819541b
Artem-PC <kiler4747@gmail.com>
2013-03-09 19:48:09 +0300

    Complete

---
+++ Eat/Mushroom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eat
+{
+	class Mushroom : IEat
+	{
+		enum Mushrooms
+		{
+			Amanita,
+			Champignon,
+			Russule
+		}
+
+		private Mushrooms mushroom;
+
+		Mushroom(Mushrooms mush)
+		{
+			mushroom = mush;
+		}
+
+		public bool Eat()
+		{
+			if (mushroom == Mushrooms.Champignon)
+				return true;
+			if (mushroom == Mushrooms.Russule)
+				return true;
+			return false;
+		}
+	}
+}
