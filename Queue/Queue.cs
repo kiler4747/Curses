c2a035cab2ffb085e6391881d25b25fcc65725c3
Artem-PC <kiler4747@gmail.com>
2013-03-09 22:00:30 +0300

    complete do

--- Queue/Queue.cs
+++ Queue/Queue.cs
@@ -7,5 +7,31 @@ namespace Queue
 {
 	class Queue
 	{
+		private QueueElem head;
+		private QueueElem end;
+
+		public void Add(int date)
+		{
+			QueueElem newElem = new QueueElem();
+			newElem.Date = date;
+
+			if (end == null)
+				head = end = newElem;
+			else
+			{
+				end.Next = newElem;
+				end = newElem;
+			}
+		}
+
+		public int Pull()
+		{
+			if (head == null)
+				throw new NullReferenceException("head in null");
+			QueueElem tmp = head;
+			head = head.Next;
+			tmp.Next = null;
+			return tmp.Date;
+		}
 	}
 }
--- Queue/QueueElem.cs
+++ Queue/QueueElem.cs
@@ -7,5 +7,20 @@ namespace Queue
 {
 	class QueueElem
 	{
+		private int date;
+
+		public int Date
+		{
+			get { return date; }
+			set { date = value; }
+		}
+
+		public QueueElem Next
+		{
+			get { return next; }
+			set { next = value; }
+		}
+
+		private QueueElem next = null;
 	}
 }
