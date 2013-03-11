3f9df4e95f9e7c7f2f077eaf62384ad1685c0e35
Artem-PC <kiler4747@gmail.com>
2013-03-11 21:55:53 +0300

    isOperator, isDelimeter, getPriority

--- StackCalc/Calc.cs
+++ StackCalc/Calc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -7,5 +8,40 @@ namespace StackCalck
 {
 	class Calc
 	{
+		static bool IsOperator(char c)
+		{
+			if (("+-*/^()".IndexOf(c)) != -1)
+				return true;
+			return false;
+		}
+
+		static bool IsDelimeter(char c)
+		{
+			if ((" =".IndexOf(c)) != -1)
+				return true;
+			return false;
+		}
+
+		static byte GetPriority(char c)
+		{
+			switch (c)
+			{
+				case '(':
+					return 0;
+				case ')':
+					return 1;
+				case '+':
+					return 2;
+				case '-':
+					return 3;
+				case '*':
+				case '/':
+					return 4;
+				case '^':
+					return 5;
+				default:
+					return 6;
+			}
+		}
 	}
 }
