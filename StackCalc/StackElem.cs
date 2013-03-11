a2906158fc891970091b5d9b389d79221157a6bf
Artem-PC <kiler4747@gmail.com>
2013-03-11 20:49:41 +0300

    Finish

---
+++ StackCalc/StackElem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StackCalck
+{
+	class StackElem<T>
+	{
+		private T data;
+		private StackElem<T> next; 
+
+
+		public T Data
+		{
+			get { return data; }
+			set { data = value; }
+		}
+
+		public StackElem<T> Next
+		{
+			get { return next; }
+			set { next = value; }
+		}
+	}
+}
