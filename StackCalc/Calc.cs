8e514afed8291820a338bfbf71b979ddffeede6b
Artem-PC <kiler4747@gmail.com>
2013-03-11 22:23:37 +0300

    GetExpression

--- StackCalc/Calc.cs
+++ StackCalc/Calc.cs
@@ -43,5 +43,56 @@ namespace StackCalck
 					return 6;
 			}
 		}
+
+		static string GetExpression(string input)
+		{
+			string output = "";
+			Stack<char> operStack = new Stack<char>();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (IsDelimeter(input[i]))
+					continue;
+				if (char.IsDigit(input[i]))
+				{
+					while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+					{
+						output += input[i];
+						i++;
+						if (i == input.Length)
+							break;
+					}
+					i--;
+					output += ' ';
+				}
+
+				if (IsOperator(input[i]))
+				{
+					if (input[i] == '(')
+						operStack.Push(input[i]);
+					else if (input[i] == ')')
+					{
+						char s = operStack.Pop();
+						while (s != '(')
+						{
+							output += s + ' ';
+							s = operStack.Pop();
+						}
+					}
+					else
+					{
+						if (!operStack.IsEmpty)
+							if (GetPriority(input[i]) <= GetPriority(operStack.Peek()))
+								output += operStack.Pop() + ' ';
+						operStack.Push(input[i]);
+					}
+				}
+			}
+			while (!operStack.IsEmpty)
+			{
+				output += operStack.Pop() + ' ';
+			}
+			return output;
+		}
 	}
 }
