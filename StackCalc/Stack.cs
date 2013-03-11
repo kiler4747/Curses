9e24d39f5f04d621a57a59f59ca6eed89fd926a8
Artem-PC <kiler4747@gmail.com>
2013-03-11 21:39:11 +0300

    Stack

--- StackCalc/Stack.cs
+++ StackCalc/Stack.cs
@@ -5,7 +5,36 @@ using System.Text;
 
 namespace StackCalck
 {
-	class Stack
+	class Stack<T>
 	{
+		private StackElem<T> head;
+
+		public void Push(T addElem)
+		{
+			StackElem<T> newElem = new StackElem<T>();
+			newElem.Data = addElem;
+			if (head == null)
+				head = newElem;
+			else
+			{
+				newElem.Next = head;
+				head = newElem;
+			}
+		}
+
+		public T Pop()
+		{
+			if (head == null)
+				throw new NullReferenceException("Stack is empty");
+			StackElem<T> tmp = head;
+			head = head.Next;
+			tmp.Next = null;
+			return tmp.Data;
+		}
+
+		public bool IsEmpty
+		{
+			get { return head == null; }
+		}
 	}
 }
