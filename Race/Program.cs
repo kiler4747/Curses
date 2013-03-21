97fca4cd36d23c86a94c70a88f8fcf58289d5890
Artem-PC <kiler4747@gmail.com>
2013-03-21 22:43:26 +0300

    Replace input
    fix timer
    fix draw progress

--- Race/Program.cs
+++ Race/Program.cs
@@ -6,13 +6,25 @@ using System.Threading.Tasks;
 
 namespace Race
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			var checkpoints = new List<int> {10, 16, 27, 39, 54, 75, 89};
-
-			var cars = new List<Car> {new Car(5), new Car(5)};
+			//Console.BufferWidth
+			Console.WriteLine("Enter the distance between checkpoints separated by spaces");
+			string str = Console.ReadLine();
+			//str = "15 27 16 39 54 75 89 41";
+			var checkpoints = (List<int>)StringToList(str);
+			
+			Console.WriteLine("Enter the number of cars");
+			string carsCountStr = Console.ReadLine();
+			int carCount;
+			var cars = new List<Car>();
+			if (!int.TryParse(carsCountStr, out carCount))
+				return;
+			//carCount = 3;
+			for (int i = 0; i < carCount; i++)
+				cars.Add(new Car(5));
 
 			Road road = new Road(checkpoints, cars);
 			road.Checkpoint += (sender, eventArgs) => { Console.WriteLine("Checkpoint is {0}", eventArgs.NumberCar); };
@@ -20,10 +32,30 @@ namespace Race
 								{
 									Console.WriteLine("Finish is {0}", eventArgs.NumberCar);
 									Console.ReadLine();
-									return;
 								};
 			road.Start();
 			while (true) ;
+		}
+
+		private static IList<int> StringToList(string str)
+		{
+			if (str == null) throw new ArgumentNullException("str");
+			string temp = "";
+			IList<int> resultList = new List<int>();
+			for (int i = 0; i < str.Length; i++)
+			{
+				if (!char.IsDigit(str[i]))
+					continue;
+				while (char.IsDigit(str[i]))
+				{
+					temp += str[i++];
+					if (i == str.Length)
+						break;
+				}
+				resultList.Add(int.Parse(temp));
+				temp = "";
+			}
+			return resultList;
 		}
 	}
 }
--- Race/Road.cs
+++ Race/Road.cs
@@ -14,6 +14,7 @@ namespace Race
 		private List<Car> cars;
 		private Timer timer;
 		private int startLine;
+		private double timeSec = 1;
 
 
 		public delegate void checkpoints(object sender, CheckpointEventArgs e);
@@ -35,14 +36,20 @@ namespace Race
 
 		Road()
 		{
-			timer = new Timer(10000);
+			timer = new Timer(timeSec * 1000);
 			timer.Elapsed += timer_Elapsed;
 		}
 
 		public Road(List<int> checkpoints, List<Car> cars)
 			: this()
 		{
-			this.checkpoint = checkpoints;
+			if (checkpoints == null) throw new ArgumentNullException("checkpoints");
+			if (cars == null) throw new ArgumentNullException("cars");
+
+			this.checkpoint = new List<int>();
+			checkpoint.Add(checkpoints[0]);
+			for (int i = 1; i < checkpoints.Count; i++ )
+				checkpoint.Add(checkpoints[i] + checkpoint.Last());
 			this.cars = cars;
 			nextChekpointCars = new List<int>();
 			for (int i = 0; i < cars.Count; i++)
@@ -55,22 +62,16 @@ namespace Race
 			startLine = Console.CursorTop;
 			int currentLine = 0;
 			Console.Write('|');
-			int persentChekcpointI = 0;
 			int prevPersentCheckpoint = 0;
-			//int persentChekcpointI = (int)((double)((double)checkpoint[0]/(double)checkpoint[checkpoint.Count - 1])*100);
-			//while (currentLine < persentChekcpointI)
-			//{
-			//	Console.Write('–');
-			//	currentLine++;
-			//}
-			for (int i = 0; i < checkpoint.Count; i++)
+			foreach (int t in checkpoint)
 			{
-				persentChekcpointI = CalculatePersantage(checkpoint[i], checkpoint[checkpoint.Count - 1]) - prevPersentCheckpoint;
+				int persentChekcpointI = CalculatePersantage(t, checkpoint[checkpoint.Count - 1]) - prevPersentCheckpoint;
 				prevPersentCheckpoint += persentChekcpointI;
 				PrintProgressPersentage(persentChekcpointI);
 				Console.Write('|');
 			}
-			Console.WriteLine();
+			for (int i = 0; i < cars.Count; i++)
+				Console.WriteLine();
 		}
 
 		int CalculatePersantage(double currentNumber, double maxNumber)
@@ -82,7 +83,7 @@ namespace Race
 
 		void PrintProgressPersentage(int value)
 		{
-			int width = Console.WindowWidth - 1;
+			int width = Console.BufferWidth;
 			int newWidth = (int)((width * value) / 100d);
 			int currentLine = 0;
 			while (currentLine + 1 < newWidth)
@@ -95,6 +96,7 @@ namespace Race
 		void PringProgress(int numberCar)
 		{
 			int curTop = Console.CursorTop;
+			Console.CursorVisible = false;
 			Console.CursorTop = startLine + numberCar + 1;
 			Console.CursorLeft = 0;
 			int distance = CalculatePersantage(cars[numberCar].Distance, checkpoint[checkpoint.Count - 1]);
@@ -102,6 +104,7 @@ namespace Race
 			Console.Write('>');
 			Console.CursorTop = curTop;
 			Console.CursorLeft = 0;
+			Console.CursorVisible = true;
 		}
 
 		private void timer_Elapsed(object sender, ElapsedEventArgs e)
