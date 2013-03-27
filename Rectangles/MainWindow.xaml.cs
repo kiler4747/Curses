788d5c139e154d6177c1fe95ff0871e13e9e95a4
Artem-PC <kiler4747@gmail.com>
2013-03-27 22:45:19 +0300

    Complite

--- Rectangles/MainWindow.xaml.cs
+++ Rectangles/MainWindow.xaml.cs
@@ -28,27 +28,28 @@ namespace Rectangles
 			InitializeComponent();
 			thrs = new List<Thread>();
 		}
-		object o = new object();
+		static object o = new object();
+		static Random rnd = new Random();
 		void ChangeSizeRectangle(object obj)
 		{
 			if (obj == null) throw new ArgumentNullException("obj");
+
 			Rectangle rct = (Rectangle)obj;
-			//double x = rct.Width;
-			bool upWidth = false;
-			Random rnd = new Random();
-			lock (o)
+			bool reverse = false;
+			
+			lock (rnd)
 			{
 				Thread.Sleep(rnd.Next(1000));
 			}
 			while (true)
 			{
-				if (!upWidth)
+				if (!reverse)
 				{
 					Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
 																				{
 																					rct.Width /= 1.05;
 																					rct.Height /= 1.05;
-																					if (rct.Width <= 10) upWidth = !upWidth;
+																					if (rct.Width <= 10) reverse = !reverse;
 																				}));
 				}
 				else
@@ -58,7 +59,7 @@ namespace Rectangles
 																					
 																					rct.Width *= 1.05;
 																					rct.Height *= 1.05;
-																					if (rct.Width >= 200) upWidth = !upWidth;
+																					if (rct.Width >= 200) reverse = !reverse;
 																				}));
 				}
 				Thread.Sleep(10);
@@ -68,6 +69,7 @@ namespace Rectangles
 		void DevideSize(Rectangle rct)
 		{
 			if (rct == null) throw new ArgumentNullException("rct");
+
 			if (rct.Width >= 100)
 			{
 				rct.Width /= 2;
@@ -87,20 +89,16 @@ namespace Rectangles
 			thrs.Clear();
 			for (int i = 0; i < 4; i++)
 				thrs.Add(new Thread(ChangeSizeRectangle));
-			Random rnd = new Random();
 			thrs[0].Start(rectangle1);
-			//Thread.Sleep(rnd.Next(1000));
 			thrs[1].Start(rectangle2);
-			//Thread.Sleep(rnd.Next(500));
 			thrs[2].Start(rectangle3);
-			//Thread.Sleep(rnd.Next(1500));
 			thrs[3].Start(rectangle4);
 		}
 
 		private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
 		{
-			for (int i = 0; i < thrs.Count; i++)
-				thrs[i].Abort();
+			foreach (Thread t in thrs)
+				t.Abort();
 		}
 	}
 }
