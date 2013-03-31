d1ee6515ea461cde71a184d110dd39592d37163a
Artem-PC <kiler4747@gmail.com>
2013-03-31 08:05:49 +0300

    add constructors, methods, properties, fields

--- TreeClasses/MainWindow.xaml.cs
+++ TreeClasses/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,13 +26,63 @@ namespace TreeClasses
 			InitializeComponent();
 		}
 
-		private string extensions = "*.exe;*.dll";
+		private string extensions = "All|*.exe;*.dll";
+		private string openedFile = @"C:\Programming\Курсы\Curses\TreeClasses\bin\Debug\TreeClasses.exe";
 
 		private void Button_Click_1(object sender, RoutedEventArgs e)
 		{
 			OpenFileDialog openDialog = new OpenFileDialog();
 			openDialog.Filter = extensions;
-			openDialog.OpenFile();
+			Nullable<bool> isSelected = openDialog.ShowDialog();
+			if (isSelected == false)
+				return;
+			openedFile = openDialog.FileName;
+			tbFilePath.Text = openedFile;
+
+			Assembly assembly = Assembly.LoadFrom(openedFile);
+			foreach (var type in assembly.GetExportedTypes())
+			{
+				TreeViewItem tvItem = new TreeViewItem
+										{
+											Header = type.Name
+										};
+				// Constructors
+				foreach (var constructorInfo in type.GetConstructors())
+				{
+					string temp = "";
+					foreach (var parameterInfo in constructorInfo.GetParameters())
+					{
+						temp += parameterInfo.ParameterType.Name + ", ";
+					}
+					if (temp.Length > 2)
+						temp = temp.Substring(0, temp.Length - 2);
+					tvItem.Items.Add(type.Name + "(" + temp + ")");
+				}
+				// Methonds
+				foreach (var methodInfo in type.GetMethods())
+				{
+					string temp = "";
+					foreach (var parameterInfo in methodInfo.GetParameters())
+					{
+						temp += parameterInfo.ParameterType.Name + ", ";
+					}
+					if (temp.Length > 2)
+						temp = temp.Substring(0, temp.Length - 2);
+					tvItem.Items.Add(methodInfo.ReturnType.Name +" " + methodInfo.Name + "(" + temp + ")");
+				}
+				// Properties
+				foreach (var propertyInfo in type.GetProperties())
+				{
+					tvItem.Items.Add(propertyInfo.PropertyType.Name + " " + propertyInfo.Name);
+				}
+				// Fields
+				foreach (var fieldInfo in type.GetFields())
+				{
+					tvItem.Items.Add(fieldInfo.FieldType.Name + " " + fieldInfo.Name);
+				}
+				tvAssemblyTree.Items.Add(tvItem);
+			}
+			
 		}
 	}
 }
