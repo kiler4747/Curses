1b1c774e5a6090a97e67b784e0579bd7e0357016
Artem-PC <kiler4747@gmail.com>
2013-04-03 12:26:11 +0300

    Complete, refactoring

--- CalcOnPlugins/MainWindow.xaml.cs
+++ CalcOnPlugins/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using PluginInterface;
 
 namespace CalcOnPlugins
@@ -25,21 +16,67 @@ namespace CalcOnPlugins
 		public MainWindow()
 		{
 			InitializeComponent();
+			operators = new List<IPlugin>();
+			LoadPlugins();
+			foreach (var plugin in operators)
+			{
+				cmOperators.Items.Add(plugin.Symbol);
+			}
+			cmOperators.SelectedIndex = 0;
 		}
 
-		private List<Type> operators;
+		private List<IPlugin> operators;
 
 		void LoadPlugins()
 		{
-			foreach (var file in Directory.EnumerateFiles(Environment.CurrentDirectory))
+			foreach (var file in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.dll"))
 			{
 				Assembly assembly = Assembly.LoadFrom(file);
 				foreach (var type in assembly.GetTypes())
 				{
 					if (type.GetInterface("IPlugin", true) == typeof (IPlugin))
-						operators.Add(type);
+					{
+						IPlugin obj = (IPlugin)Activator.CreateInstance(type);
+						operators.Add(obj);
+					}
 				}
 			}
 		}
+
+		private void tbCalculate_Click(object sender, RoutedEventArgs e)
+		{
+			Calculate();
+		}
+
+		void Calculate()
+		{
+			double a = 0, b = 0;
+			try
+			{
+				a = double.Parse(tbOperandA.Text);
+			}
+			catch (FormatException)
+			{
+				MessageBox.Show("Operand A is invalid number format");
+				return;
+			}
+			try
+			{
+				b = double.Parse(tbOperandB.Text);
+			}
+			catch (FormatException)
+			{
+				MessageBox.Show("Operand B is invalid number format");
+				return;
+			}
+			lbResult.Content = operators[cmOperators.SelectedIndex].Operation(a, b).ToString();
+		}
+
+		private void tbOperand_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			if (!Char.IsDigit(e.Text, 0) && !(e.Text[0] == ',')) 
+				e.Handled = true;
+		}
+
 	}
 }
--- Summator/Substraction.cs
+++ Summator/Substraction.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using PluginInterface;
+﻿using PluginInterface;
 
 namespace Summator
 {
